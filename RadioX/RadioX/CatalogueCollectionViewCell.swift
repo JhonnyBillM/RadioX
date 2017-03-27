228d7961dfa6bd1940c9f1bae32d42560715ef60
zelo <zelo@Lareens-MacBook-Pro.local>
2017-03-27 13:56:37 -0400

    Collection view added (currently in the testing stage).

---
+++ RadioX/RadioX/CatalogueCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CatalogueCollectionViewCell.swift
+//  RadioX
+//
+//  Created by zelo on 3/27/17.
+//  Copyright © 2017 gestionadoresDiscretos. All rights reserved.
+//
+
+import UIKit
+
+class CatalogueCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
