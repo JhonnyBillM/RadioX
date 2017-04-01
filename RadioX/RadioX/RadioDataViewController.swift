cbc119062f11d214fc91a7eb2c43e4397979ad97
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-04-01 07:45:50 -0400

    Added core data stuffs. Developing RadioDataViewController

---
+++ RadioX/RadioX/RadioDataViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RadioDataViewController.swift
+//  RadioX
+//
+//  Created by Jhonny Bill Mena on 4/1/17.
+//  Copyright © 2017 gestionadoresDiscretos. All rights reserved.
+//
+
+import UIKit
+
+class RadioDataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
