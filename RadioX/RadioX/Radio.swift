35c9131daaa73b8534e08ba8d180c000874297a5
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-04-01 07:46:33 -0400

    Added core data stuffs. Developing RadioDataViewController

--- RadioX/RadioX/Radio.swift
+++ RadioX/RadioX/Radio.swift
@@ -7,7 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class Radio: NSManagedObject {
 
+    
+//    init(){
+//        
+//    }
+
+
+    
+    
+    //We will save all radio data only the first time the user launch the App
+    
+    func saveData(){
+        
+        let context = AppDelegate.viewContext
+        let radio = NSEntityDescription.insertNewObject(forEntityName: "Radio", into: context)
+        
+        radio.setValue("01", forKeyPath: "radioID")
+        radio.setValue("Radio Intec", forKeyPath: "name")
+        radio.setValue("Santo Domingo, Intec", forKeyPath: "direction")
+        radio.setValue("radio@intec.edu.do", forKeyPath: "eMail")
+        radio.setValue("Radio Intec", forKeyPath: "facebook")
+        radio.setValue("Radio Intec", forKeyPath: "instagram")
+        radio.setValue("Radio Intec", forKeyPath: "phoneNumber")
+        radio.setValue("Radio Intec", forKeyPath: "snapchat")
+        radio.setValue("Radio Intec", forKeyPath: "twitter")
+        
+        do{
+            try context.save()
+        }catch{
+            //bueeeeno
+        }
+        
+    }
+   
+    
+    
+    
 }
