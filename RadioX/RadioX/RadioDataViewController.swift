64d2fe6bfd4afe11f4dffda4dae66f36a1497a46
Jhonny Bill Mena <jhonnybillm@gmail.com>
2017-04-06 00:40:36 -0400

    Added heartButton to catalogueView

--- RadioX/RadioX/RadioDataViewController.swift
+++ RadioX/RadioX/RadioDataViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import FaveButton
 
+func color(_ rgbColor: Int) -> UIColor{
+    return UIColor(
+        red:   CGFloat((rgbColor & 0xFF0000) >> 16) / 255.0,
+        green: CGFloat((rgbColor & 0x00FF00) >> 8 ) / 255.0,
+        blue:  CGFloat((rgbColor & 0x0000FF) >> 0 ) / 255.0,
+        alpha: CGFloat(1.0)
+    )
+}
 
-class RadioDataViewController: UIViewController {
+class RadioDataViewController: UIViewController, FaveButtonDelegate{
 
     var showToDetail = Show()
     
@@ -21,6 +30,28 @@ class RadioDataViewController: UIViewController {
     
     @IBOutlet weak var descriptionTextView: UITextView!
    
+
+    @IBOutlet weak var heartButton: FaveButton!
+    
+    
+    
+    let colors = [
+        DotColors(first: color(0x7DC2F4), second: color(0xE2264D)),
+        DotColors(first: color(0xF8CC61), second: color(0x9BDFBA)),
+        DotColors(first: color(0xAF90F4), second: color(0x90D1F9)),
+        DotColors(first: color(0xE9A966), second: color(0xF8C852)),
+        DotColors(first: color(0xF68FA7), second: color(0xF6A2B8))
+    ]
+    
+    func faveButton(_ faveButton: FaveButton, didSelected selected: Bool){
+    }
+    
+    func faveButtonDotColors(_ faveButton: FaveButton) -> [DotColors]?{
+        if( faveButton === heartButton){
+            return colors
+        }
+        return nil
+    }
     
     
     override func viewDidLoad() {
@@ -35,8 +66,8 @@ class RadioDataViewController: UIViewController {
         showNameLabel.text = showToDetail.name!
         descriptionTextView.text = showToDetail.descrip!
         
-        
-        
+//        heartButton.setImage(UIImage(named:"heart")!, for: .normal)
+//        
         
     }
     
