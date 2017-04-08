aaa7beb6fa07028b7e568a577891ea13b0ef726f
Lareen Melo <lareenmelo@gmail.com>
2017-04-07 21:08:06 -0400

    Settings is working thnks to abraham.

--- RadioX/RadioX/SettingsTableViewController.swift
+++ RadioX/RadioX/SettingsTableViewController.swift
@@ -28,17 +28,17 @@ class SettingsTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
+//    // MARK: - Table view data source
+//
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 4
+//    }
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
---
+++ RadioX/SettingsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  SettingsViewController.swift
+//  
+//
+//  Created by fitmap on 4/7/17.
+//
+//
+
+//import UIKit
+//
+//class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+//    let cells = ["thing one", "thing two", "thing three"]
+//  
+//    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+//    {
+//        return(cells.count)
+//    }
+//    
+//    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+//    {
+//        let celda = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+//        celda.textLabel?.text = cells[indexPath.row]
+//        return(celda)
+//        
+//    }
+//    
+//
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//	
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//    
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destinationViewController.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
